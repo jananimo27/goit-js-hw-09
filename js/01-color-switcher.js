d627767e03eee159f30d1ad312237b51297eaef7
jananimo27 <147221349+jananimo27@users.noreply.github.com>
2024-04-22 01:52:03 +0800

    1

--- js/01-color-switcher.js
+++ js/01-color-switcher.js
@@ -1,17 +1,15 @@
-// Selecting the Start and Stop buttons
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 
-let intervalId = null; // To hold the interval ID for the setInterval function
+let intervalId = null; 
 
-// Function to generate a random hex color
+
 function getRandomColor() {
     return `#${Math.floor(Math.random() * 0xffffff).toString(16).padStart(6, '0')}`;
 }
 
-// Function to start changing the background color
 function startColorChange() {
-    if (!intervalId) { // Only set the interval if it's not already set
+    if (!intervalId) { 
         intervalId = setInterval(() => {
             document.body.style.backgroundColor = getRandomColor();
         }, 1000);
@@ -21,10 +19,9 @@ function startColorChange() {
 // Function to stop changing the background color
 function stopColorChange() {
     clearInterval(intervalId);
-    intervalId = null; // Reset the interval ID
+    intervalId = null; 
 }
 
-// Event listeners for the buttons
 startButton.addEventListener('click', startColorChange);
 stopButton.addEventListener('click', stopColorChange);
 
